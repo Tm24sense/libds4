3dc3a992fbb50367a4e957f14e3bb32e3b7a0a40
Tm24sense <tayiemallah57@gmail.com>
2026-01-27 21:05:05 +0200

    This is important

--- include/ds4/ds4_output.h
+++ include/ds4/ds4_output.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include <stdbool.h>
+#include "ds4/ds4_handle.h"
 
 typedef struct ds4_output_report {
     uint8_t report[32];
@@ -15,4 +16,4 @@ ds4_message ds4_begin_message(void);
 void ds4_set_vibration(ds4_message*, uint8_t right_motor, uint8_t left_motor);
 void ds4_set_led(ds4_message*, uint8_t r, uint8_t g, uint8_t b);
 void ds4_enable_flash(ds4_message*, uint8_t flash_on, uint8_t flash_off, bool on);
-int ds4_send_commands(struct ds4_handle*, ds4_message*);
+int ds4_send_commands(ds4_handle* dev, ds4_message* msg);
