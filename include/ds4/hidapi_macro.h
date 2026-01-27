6fd33e112b10c180b295b6904ed664c0bbe273fe
Tm24sense <tayiemallah57@gmail.com>
2026-01-27 20:15:57 +0200

    Bug fixes

--- include/ds4/hidapi_macro.h
+++ include/ds4/hidapi_macro.h
@@ -1,3 +1,13 @@
-#pragma once
+#ifndef HIDAPI_MACRO_H
+#define HIDAPI_MACRO_H
 
-#include <hidapi/hidapi.h>
+// If we are on Windows, we usually need the prefix
+#if defined(_WIN32) || defined(_WIN64)
+    #include <hidapi/hidapi.h>
+#else
+    // On Linux/macOS, the include path usually points 
+    // directly to the header's folder
+    #include <hidapi.h>
+#endif
+
+#endif
